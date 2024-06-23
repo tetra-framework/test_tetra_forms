c0cd0874a462b65097371c7de0b0ff250b442dc4
Christian González <christian.gonzalez@nerdocs.at>
2024-06-23 17:18:59 +0200

    set str methods of models

--- main/models.py
+++ main/models.py
@@ -5,7 +5,13 @@ class Person(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"
+
 
 class Book(models.Model):
     name = models.CharField(max_length=100)
     author = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} ({self.author})"
