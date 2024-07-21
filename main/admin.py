15f72b00a78eb4b9d1661414901dbc9962dd3b61
Christian González <christian.gonzalez@nerdocs.at>
2024-07-22 00:22:49 +0200

    add address to admin

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from main.models import Person, Book
+from main.models import Person, Book, PersonAddress
 
 admin.site.register(Person)
 admin.site.register(Book)
+admin.site.register(PersonAddress)
